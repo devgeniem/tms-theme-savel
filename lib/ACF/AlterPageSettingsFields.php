a9153c54f96c1af91b91ffc112ebbabd077da2e5
Erik Vänttinen <eepi1994@hotmail.com>
2025-01-24 09:43:35 +0200

    TMS-1098: Add image-shape choices for pages hero-image

--- lib/ACF/AlterPageSettingsFields.php
+++ lib/ACF/AlterPageSettingsFields.php
@@ -28,6 +28,21 @@ class AlterPageSettingsFields {
     public function remove_hero_overlay_setting( $group ) {
         try {
             if ( $group->get_key() === 'fg_page_settings' ) {
+                $s = [
+                    'image_shape' => [
+                        'title'        => 'Kuvan muoto',
+                        'instructions' => 'Valitse kuvan muoto',
+                    ],
+                ];
+
+                $image_shape = ( new \Geniem\ACF\Field\Select( $s['image_shape']['title'] ) )
+                ->set_key( 'fg_page_settings_image_shape' )
+                ->set_name( 'image_shape' )
+                ->set_instructions( $s['image_shape']['instructions'] )
+                ->set_choices( \apply_filters( 'tms_afc_image_shape_choices', [] ) );
+
+                $group->add_field( $image_shape );
+
                 $group->remove_field( 'fg_page_settings_remove_overlay' );
             }
         }
--- lib/ThemeSupports.php
+++ lib/ThemeSupports.php
@@ -24,5 +24,27 @@ class ThemeSupports implements Controller {
             'tms/theme/remove_custom_links',
             '__return_false'
         );
+
+        \add_filter( 'tms_afc_image_shape_choices', \Closure::fromCallable( [
+            $this,
+            'acf_image_shape_choices',
+        ] ), 10, 3 );
+    }
+
+        /**
+     * ACF layout brand color field choices
+     *
+     * @return string[]
+     */
+    private function acf_image_shape_choices() : array {
+        return [
+            'default'                         => 'Ei muotoa',
+            'shape shape--summertime-sadness' => 'Apila',
+            'shape shape--drop'               => 'Pisara',
+            'shape shape--soft-flower'        => 'Nelireunainen kukka',
+            'shape shape--flower'             => 'Kukka',
+            'shape shape--polygon'            => 'Monikulmio',
+            'shape shape--rainbow'            => 'Sateenkaari',
+        ];
     }
 }
